a56df56e4170d70d5f10623cd85ad5081e3c9759
Alexander Gutev <alex.gutev@gmail.com>
2018-07-25 09:21:35 +0200

    Add let-over-lambda utilities used in this project.
    Only the utilities used in this project, which do not depend on
    named-readtables are added.

---
+++ src/common/let-over-lambda.lisp
@@ -0,0 +1,140 @@
+;; This file contains utility functions and macros, taken from
+;; let-over-lambda, which are used in the cl-environments project.
+;; The only modification is that the functions and macros have been
+;; moved from the :let-over-lambda package to the
+;; :cl-environments.util package.
+
+;; Antiweb (C) Doug Hoyte
+
+;; This is a "production" version of LOL with bug-fixes
+;; and new features in the spirit of the book.
+
+;; See http://letoverlambda.com
+
+;; This is the source code for the book
+;; _Let_Over_Lambda_ by Doug Hoyte.
+;; This code is (C) 2002-2008, Doug Hoyte.
+;;
+;; You are free to use, modify, and re-distribute
+;; this code however you want, except that any
+;; modifications must be clearly indicated before
+;; re-distribution. There is no warranty,
+;; expressed nor implied.
+;;
+;; Attribution of this code to me, Doug Hoyte, is
+;; appreciated but not necessary. If you find the
+;; code useful, or would like documentation,
+;; please consider buying the book!
+
+;; Modifications by "the Phoeron" Colin J.E. Lupton, 2012--2014
+;; - Support for ASDF/Quicklisp
+;; - Cheap hacks to support new Backquote implementation in SBCL v1.2.2
+
+(in-package :cl-environments.util)
+
+#+sbcl
+(eval-when (:compile-toplevel :execute)
+  (handler-case
+      (progn
+        (sb-ext:assert-version->= 1 2 2)
+        (setq *features* (remove 'old-sbcl *features*)))
+    (error ()
+      (pushnew 'old-sbcl *features*))))
+
+(defun group (source n)
+  (if (zerop n) (error "zero length"))
+  (labels ((rec (source acc)
+             (let ((rest (nthcdr n source)))
+               (if (consp rest)
+                   (rec rest (cons
+                               (subseq source 0 n)
+                               acc))
+                   (nreverse
+                     (cons source acc))))))
+    (if source (rec source nil) nil)))
+
+(eval-when (:compile-toplevel :execute :load-toplevel)
+  (defun mkstr (&rest args)
+    (with-output-to-string (s)
+      (dolist (a args) (princ a s))))
+
+  (defun symb (&rest args)
+    (values (intern (apply #'mkstr args))))
+
+  (defun flatten (x)
+    (labels ((rec (x acc)
+                  (cond ((null x) acc)
+                        #+(and sbcl (not old-sbcl))
+                        ((typep x 'sb-impl::comma) (rec (sb-impl::comma-expr x) acc))
+                        ((atom x) (cons x acc))
+                        (t (rec
+                             (car x)
+                             (rec (cdr x) acc))))))
+      (rec x nil)))
+
+  (defun g!-symbol-p (s)
+    (and (symbolp s)
+         (> (length (symbol-name s)) 2)
+         (string= (symbol-name s)
+                  "G!"
+                  :start1 0
+                  :end1 2)))
+
+  (defun o!-symbol-p (s)
+    (and (symbolp s)
+         (> (length (symbol-name s)) 2)
+         (string= (symbol-name s)
+                  "O!"
+                  :start1 0
+                  :end1 2)))
+
+  (defun o!-symbol-to-g!-symbol (s)
+    (symb "G!"
+          (subseq (symbol-name s) 2))))
+
+(defmacro defmacro/g! (name args &rest body)
+  (let ((syms (remove-duplicates
+               (remove-if-not #'g!-symbol-p
+                              (flatten body)))))
+    (multiple-value-bind (body declarations docstring)
+        (parse-body body :documentation t)
+      `(defmacro ,name ,args
+         ,@(when docstring
+             (list docstring))
+         ,@declarations
+         (let ,(mapcar
+                (lambda (s)
+                  `(,s (gensym ,(subseq
+                                 (symbol-name s)
+                                 2))))
+                syms)
+           ,@body)))))
+
+(defmacro defmacro! (name args &rest body)
+  (let* ((os (remove-if-not #'o!-symbol-p (flatten args)))
+         (gs (mapcar #'o!-symbol-to-g!-symbol os)))
+    (multiple-value-bind (body declarations docstring)
+        (parse-body body :documentation t)
+      `(defmacro/g! ,name ,args
+         ,@(when docstring
+            (list docstring))
+         ,@declarations
+         `(let ,(mapcar #'list (list ,@gs) (list ,@os))
+            ,(progn ,@body))))))
+
+(defmacro defun! (name args &body body)
+  (let ((syms (remove-duplicates
+               (remove-if-not #'g!-symbol-p
+                              (flatten body)))))
+    (multiple-value-bind (body declarations docstring)
+        (parse-body body :documentation t)
+      `(defun ,name ,args
+         ,@(when docstring
+             (list docstring))
+         ,@declarations
+         (let ,(mapcar (lambda (s)
+                         `(,s (gensym ,(subseq (symbol-name s)
+                                               2))))
+                       syms)
+           ,@body)))))
+
