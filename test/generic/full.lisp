859bbd8383d09b938a42f0eb23ad74315096cda2
Alexander Gutev <alex.gutev@gmail.com>
2019-04-19 08:02:43 +0200

    Add tests for special variables, LET* and LOCALLY forms.

--- test/generic/full.lisp
+++ test/generic/full.lisp
@@ -120,7 +120,7 @@
 (plan 3)
 
 (subtest "Test VARIABLE-INFORMATION"
-  (subtest "Test LET forms"
+  (subtest "Test lexical binding using LET"
     (test-form
      "Single LET form"
 
@@ -177,6 +177,168 @@
 	 (var-info x y)))
 
      (:lexical t ((type . number) (dynamic-extent . nil)))
-     (:lexical t ((type . number))))))
+     (:lexical t ((type . number))))
+
+    (test-form
+     "Free Declaration"
+
+     (let ((a 1))
+       (declare (type integer b))
+
+       (var-info b))
+
+     (nil nil ((type . integer))))
+
+    (test-form
+     "Additional declarations to outer LET binding"
+
+     (let ((a 1))
+       (declare (type integer a))
+
+       (append
+	(var-info a)
+
+	(let ((b 2))
+	  (declare (dynamic-extent a))
+	  (var-info a))))
+
+     (:lexical t ((type . integer) (dynamic-extent . nil)))
+     (:lexical t ((type . integer) (dynamic-extent . t)))))
+
+  (subtest "Test special bindings and special declarations"
+    (test-form
+     "Global Special Variables"
+
+     (progn
+       (defvar *test* 1)
+       (declaim (type integer *test*))
+       (var-info *test*))
+
+     (:special nil ((type . integer))))
+
+    (test-form
+     "Predefined Global Special Variables"
+
+     (var-info *standard-input*)
+     (:special nil nil))
+
+    (test-form
+     "Shadowing Special Variables"
+
+     (progn
+       (defvar *test* 1)
+       (declaim (type integer *test*))
+
+       (let ((*test* 2))
+	 (var-info *test*)))
+
+     (:special t ((type . nil))))
+
+    (test-form
+     "Shadowing Predefined Special Variables"
+
+     (let ((*standard-output* (make-broadcast-stream)))
+       (var-info *standard-output*))
+
+     (:special t nil))
+
+    (test-form
+     "Special Declarations"
+
+     (let ((x 1))
+       (declare (type fixnum x))
+       (declare (special x))
+
+       (var-info x))
+
+     (:special t ((type . fixnum))))
+
+    (test-form
+     "LET nested in LET with SPECIAL declaration"
+
+     (let ((x 1))
+       (declare (type fixnum x))
+       (declare (special x))
+
+       (let ((x #\a))
+	 (var-info x)))
+
+     (:lexical t ((type . nil)))))
+
+  (subtest "Test LET* forms"
+    (test-form
+     "LET init-form environment"
+
+     (let ((x 1) (y (var-info x)))
+       (declare (type integer x))
+       y)
+
+     (nil nil ((type . nil))))
+
+    (test-form
+     "LET* init-form environment"
+
+     (let* ((x 1) (y (var-info x z)) (z 2))
+       (declare (type integer x z))
+       y)
+
+     (:lexical t ((type . integer)))
+     (nil nil ((type . nil))))
+
+    (test-form
+     "LET* body environment"
+
+     (let* ((x 1) (y 2) (z 3))
+       (declare (type integer x z))
+       (declare (type fixnum y) (ignore y))
+
+       (var-info x y z))
+
+     (:lexical t ((type . integer)))
+     (:lexical t ((type . fixnum) (ignore . t)))
+     (:lexical t ((type . integer)))))
+
+  (subtest "Test LOCALLY forms"
+    (test-form
+     "Additional declarations to LET variable"
+
+     (let ((x 4))
+       (declare (dynamic-extent x))
+
+       (append
+	(var-info x)
+
+	(locally
+	    (declare (type number x))
+	  (var-info x))))
+
+     (:lexical t ((dynamic-extent . t) (type . nil)))
+     (:lexical t ((dynamic-extent . t) (type . number))))
+
+    (test-form
+     "Free declaration"
+
+     (locally
+	 (declare (type integer a))
+
+       (var-info a))
+
+     (nil nil ((type . integer))))
+
+    (test-form
+     "Special declaration"
+
+     (let ((a 1))
+       (declare (type integer a))
+
+       (append
+	(var-info a)
+
+	(locally (declare (special a b))
+	  (var-info a b))))
+
+     (:lexical t ((type . integer)))
+     (:special t ((type . integer)))
+     (:special nil nil))))
 
 (finalize)
